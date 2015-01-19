764fa5c8b39d5873426ea4f5593f96b9e412b6d5
Alex Hagen <ahagen@purdue.edu>
2015-01-19 14:27:12 -0500

    added a qt class

---
+++ ah_udp_client_qt.cpp
@@ -0,0 +1,45 @@
+#include "ah_udp_client_qt.h"
+#include <QDebug>
+
+ah_udp_client_qt::ah_udp_client_qt(void){
+    cont = true;
+    qDebug() << "starting socket";
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+    bzero((char *)&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(PORT);
+    addrlen = sizeof(addr);
+
+    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+    mreq.imr_multiaddr.s_addr = inet_addr(GROUP);
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+        &mreq, sizeof(mreq)) < 0) {
+        perror("setsockopt mreq");
+        exit(1);
+    }
+}
+
+void ah_udp_client_qt::start(void) {
+    while (cont) {
+        qDebug() << "started";
+        cnt = recvfrom(sock, message, sizeof(message), 0,
+            (struct sockaddr *) &addr, &addrlen);
+        if (cnt < 0) {
+            printf("error");
+            perror("recvfrom");
+            exit(1);
+        } else if (cnt > 0) {
+            emit data_ready(message);
+        }
+    }
+};
+
---
+++ ah_udp_client_qt.h
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <QObject>
+
+#define PORT 6000
+#define GROUP "239.1.1.1"
+
+#define BUFSIZE 2048
+
+class ah_udp_client_qt : public QObject {
+
+    Q_OBJECT
+
+public:
+    ah_udp_client_qt(void);
+    void start(void);
+
+signals:
+    void data_ready(char* msg);
+
+private:
+    struct sockaddr_in addr;
+    socklen_t addrlen;
+    int sock, cnt;
+    struct ip_mreq mreq;
+    char message[BUFSIZE];
+    bool cont;
+};
