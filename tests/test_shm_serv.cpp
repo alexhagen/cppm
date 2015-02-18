cb663f11007c9e0d7babc9a1880c9b04e8b58b1d
Alex Hagen <ahagen@purdue.edu>
2015-02-18 12:30:01 -0500

    working object oriented shm object

---
+++ tests/test_shm_serv.cpp
@@ -0,0 +1,12 @@
+#include "ah_shm.h"
+
+int main(void){
+
+	shm_server<int> *some_var = new shm_server<int>(5678);
+	some_var->set(5);
+
+	while (*some_var->shm != '*')
+        sleep(1);
+
+	return 0;
+}
