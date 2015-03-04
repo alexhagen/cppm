e18c4af2989fadc6e59054e2821ad79750b44090
Alex Hagen <ahagen@purdue.edu>
2015-03-04 12:18:12 -0500

    wrote first revision of code

--- ah_signal.h
+++ ah_signal.h
@@ -2,31 +2,59 @@
 #define ahsignal_h
 #include <signal.h>
 
+void signalHandler(int sig, siginfo_t *si, void *uc){
+    timer *object = (timer *)si->si_value.sival_ptr;
+    object->sig->emit();
+}
+
 class ah_signal {
 public:
 	ah_signal(void);
-	~ah_signal(void);
 	void set(int);
-	void action(void);
+	void action(void*);
 
-public:
-	struct sigevent te;
+private:
+	struct sigevent e;
 	struct sigaction sa;
+	int signum;
 };
 
 ah_signal::ah_signal(void){
-
+	sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = &signalHandler;
+    sigemptyset(&sa.sa_mask);
 }
 
-ah_signal::~ah_signal(void){
-
+ah_signal::ah_signal(int _signum,void* act){
+	sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = &signalHandler;
+    sigemptyset(&sa.sa_mask);
+    set(_signum);
+    action(act);
 }
 
-void ah_signal::set(int signal){
-
+void ah_signal::set(int _signum){
+	signum = _signum;
+    // connecting the hardware signal SIGALRM to the static function
+    // signalHandler, can't do much in signalHandler so we just emit
+    // a signal
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = &signalHandler;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(signum, &sa, NULL) == -1)
+    {
+        return;
+    }
+    /* Set and enable alarm */
+    e.sigev_notify = SIGEV_SIGNAL;
+    e.sigev_signo = sigNo;
+    e.sigev_value.sival_ptr = (void*) this;
 }
 
-void ah_signal::action(void){
-	
+void ah_signal::action(void* act){
+    /* Set and enable alarm */
+    e.sigev_notify = SIGEV_SIGNAL;
+    e.sigev_signo = signum;
+    e.sigev_value.sival_ptr = (void*) act;
 }
 #endif
