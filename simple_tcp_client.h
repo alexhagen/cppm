fd733dd60ce30a4b949706ed73deb8655f716f96
Alex Hagen <ahagen@purdue.edu>
2015-05-22 14:15:54 -0400

    made a "simple_tcp_client" library that doesnt use sigslot"

--- simple_tcp_client.h
+++ simple_tcp_client.h
@@ -1,6 +1,6 @@
 #pragma GCC diagnostic ignored "-Wwrite-strings"
-#ifndef ah_tcp_client_h
-#define ah_tcp_client_h
+#ifndef simple_tcp_client_h
+#define simple_tcp_client_h
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +13,6 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string>
-#include "sigslot.h"
 
 using namespace std;
 
@@ -28,14 +27,13 @@ using namespace std;
 #define ERR_INFO	5
 #define ERR_DEBUG	6
 
-/*                            AH TCP Client class                             */
+/*                            Simple TCP Client class                             */
 /*                             Author: Alex Hagen                             */
 /* This client is a c implementation of the c directives to write to a tcp pipe
 	and then wait for a response (or not).  I have tried to encapsulate and
-	overload everything, and allow for connections of signals and slots to allow
-	for advanced configuration.  */
+	overload everything.  */
 
-class ah_tcp_client : public sigslot::has_slots<>{
+class simple_tcp_client {
 public:
 	int sd, rc, length = sizeof(int);
 	struct sockaddr_in serveraddr;
@@ -45,24 +43,24 @@ public:
 	bool data_ready_sig = false;
 	struct hostent *hostp;
 	char err_str[255],data[BufferLength];
-	sigslot::signal2<int,char*> error;
-	sigslot::signal1<char*> data_ready;
 	int port = DEFAULTPORT;
 	unsigned long int ipAddr = INADDR_NONE;
 
-	ah_tcp_client(void);
+	simple_tcp_client(void);
 	void tcp_connect(char* _ipAddr, int _port);
 	void disconnect(void);
 	void send_data(char*);
+	void accumulate(int);
+	void accumulate(char);
 };
 
-ah_tcp_client::ah_tcp_client(void){
+simple_tcp_client::simple_tcp_client(void){
 	// get a socket descriptor
 	if((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		error.emit(ERR_CRIT,"socket not created.");
+		//error.emit(ERR_CRIT,"socket not created.");
 		exit(-1);
 	} else {
-		error.emit(ERR_DEBUG,"Socket created.");
+		//error.emit(ERR_DEBUG,"Socket created.");
 	}
 
 	// set up the server addresses using c commands
@@ -70,7 +68,7 @@ ah_tcp_client::ah_tcp_client(void){
 	serveraddr.sin_family = AF_INET;
 }
 
-void ah_tcp_client::tcp_connect(char* _ipAddr, int _port){
+void simple_tcp_client::tcp_connect(char* _ipAddr, int _port){
 	// find the ipAddr from the string
 	ipAddr = inet_addr(_ipAddr);
 	// get the port
@@ -86,7 +84,7 @@ void ah_tcp_client::tcp_connect(char* _ipAddr, int _port){
 			char err_str[255];
 			sprintf(err_str,"HOST NOT FOUND --> ");
 			sprintf(err_str,"h_errno = %d\n",h_errno);
-			error.emit(ERR_ERR,err_str);
+			//error.emit(ERR_ERR,err_str);
 			close(sd);
 			exit(-1);
 		}
@@ -96,55 +94,90 @@ void ah_tcp_client::tcp_connect(char* _ipAddr, int _port){
 	// connect() to server. */
 	if((rc = connect(sd, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) < 0) {
 		//perror("Client-connect() error");
-		error.emit(ERR_ERR,"couldn't connect.");
+		//error.emit(ERR_ERR,"couldn't connect.");
 		close(sd);
 		exit(-1);
 	} else {
-		error.emit(ERR_DEBUG,"connected");
+		//error.emit(ERR_DEBUG,"connected");
 	}
 
 }
 
-void ah_tcp_client::disconnect(void){
+void simple_tcp_client::disconnect(void){
 	close(sd);
 }
 
-void ah_tcp_client::send_data(char* msg){
+void simple_tcp_client::accumulate(int num){
+	memset(buffer,0,sizeof(buffer));
+	rc = recv(sd, &buffer,sizeof(buffer),0);
+	while (rc < num){
+		rc = recv(sd, &buffer,sizeof(buffer),0);
+		printf("waiting...\n");
+	}
+	memcpy(this->data,buffer,num*sizeof(this->data[0]));
+	this->data_ready_sig = true;
+}
+
+void simple_tcp_client::accumulate(char start_byte){
+	char byte = 0;
+	memset(buffer,0,sizeof(buffer));
+	rc = recv(sd,&byte,sizeof(byte),0);
+	printf("rc = %d\n",rc);
+	while (byte != 0x4242){
+		rc = recv(sd,&byte,sizeof(byte),0);
+		printf("recieved byte %c\n",byte);
+		printf("rc = %d\n",rc);
+		printf("compare to %c\n",0x4242);
+	}
+	int i = 0;
+	while (byte != start_byte){
+		rc = recv(sd,&byte,sizeof(byte),0);
+		buffer[i] = byte;
+		i = i+1;
+	}
+	printf("got %d bytes between start characters, %c\n",i,0x4242);
+	memcpy(this->data,buffer,i*sizeof(this->data[0]));
+}
+
+void simple_tcp_client::send_data(char* msg){
 	this->data_ready_sig = false;
 	// write string to the server
 	rc = write(sd, msg, strlen(msg));
 	 
 	if(rc < 0) {
-		error.emit(ERR_ERR,"couldn't write to client");
+		//error.emit(ERR_ERR,"couldn't write to client");
 		rc = getsockopt(sd, SOL_SOCKET, SO_ERROR, &temp, (socklen_t*)&length);
 		if(rc == 0) {
 			/* Print out the asynchronously received error. */
 			errno = temp;
-			error.emit(ERR_ERR,"Socket_option was");
+			//error.emit(ERR_ERR,"Socket_option was");
 		}
 		close(sd);
 		exit(-1);
 	} else {
 		sprintf(this->err_str,"Client-write() is OK\n");
-		sprintf(this->err_str,"String successfully sent lol!\n");
-		error.emit(ERR_DEBUG,err_str);
+		sprintf(this->err_str,"String successfully sent!\n");
+		//error.emit(ERR_DEBUG,err_str);
 	}
 	
 	memset(buffer,0,sizeof(buffer));
 	if (rc = recv(sd, &buffer,sizeof(buffer),0) <= 0){
 		if(rc < 0) {
-			error.emit(ERR_ERR,"client read error");
+			//error.emit(ERR_ERR,"client read error");
 			close(sd);
 			exit(-1);
 		} else if (rc == 0) {
-			error.emit(ERR_ERR,"Server program closed.");
+			//error.emit(ERR_ERR,"Server program closed.");
 			close(sd);
 			exit(-1);
 		}
 	}
-	error.emit(ERR_DEBUG,"Client-read() is OK");
 	sprintf(this->data,buffer);
-	data_ready.emit(buffer);
+	printf("number of bytes found is %d.\n",rc);
+	printf("these bytes are: %s.\n",this->data);
+	//error.emit(ERR_DEBUG,"Client-read() is OK");
+	
+	//data_ready.emit(buffer);
 	this->data_ready_sig = true;
 }
 
