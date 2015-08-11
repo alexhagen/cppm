c1bf5cef93dc8ddc82bd354364c06da3ffd11796
Alex Hagen <ahagen@purdue.edu>
2015-08-11 15:32:28 -0400

    created prototypes

---
+++ ah_cll.h
@@ -0,0 +1,39 @@
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+#ifndef ah_cll_h
+#define ah_cll_h
+
+/*                        AH Circular Linked List Class                       */
+/*                             Author: Alex Hagen                             */
+/* This is an implementation of a linked list class that is circular for fast
+and efficient data storage.  */
+
+tmeplate <class T> class ah_cll {
+public:
+  int length;
+
+  ah_cll(T, int);
+  void add(T);
+  void clear(void);
+  T pop(void);
+
+private:
+
+};
+
+tmeplate <class T> ah_cll::ah_cll(T x,int length){
+
+}
+
+tmeplate <class T> void ah_cll::ah_cll(T x){
+
+}
+
+tmeplate <class T> void ah_cll::clear(void){
+
+}
+
+tmeplate <class T> T ah_cll:pop(void){
+  return x;
+}
+
+#endif
