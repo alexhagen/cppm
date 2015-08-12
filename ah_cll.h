050a6e1497e7569c42719250baa4d1bd1cac8ab6
Alex Hagen <ahagen@purdue.edu>
2015-08-12 09:05:08 -0400

    made node data structure

--- ah_cll.h
+++ ah_cll.h
@@ -7,9 +7,25 @@
 /* This is an implementation of a linked list class that is circular for fast
 and efficient data storage.  */
 
-tmeplate <class T> class ah_cll {
+template <class T> class cll_node {
+public:
+  T object;
+  void* next;
+  void* last;
+
+  cll_node(T,void*,void*);
+}
+
+template <class T> cll_node::cll_node(T _object,void* _next,void* _last){
+  object = _object;
+  next = _next;
+  last = _last;
+}
+
+template <class T> class ah_cll {
 public:
   int length;
+  void* first;
 
   ah_cll(T, int);
   void add(T);
@@ -20,19 +36,20 @@ private:
 
 };
 
-tmeplate <class T> ah_cll::ah_cll(T x,int length){
+template <class T> ah_cll::ah_cll(T x,int length){
+  // First, allocate a place for the object
+  first = malloc(sizeof(cll_node))
+}
+
+template <class T> void ah_cll::ah_cll(T x){
 
 }
 
-tmeplate <class T> void ah_cll::ah_cll(T x){
+template <class T> void ah_cll::clear(void){
 
 }
 
-tmeplate <class T> void ah_cll::clear(void){
-
-}
-
-tmeplate <class T> T ah_cll:pop(void){
+template <class T> T ah_cll:pop(void){
   return x;
 }
 
