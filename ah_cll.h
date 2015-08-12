d8a0268c3c3c528819a3994a1fffbaa2e45ed8d4
Alex Hagen <ahagen@purdue.edu>
2015-08-12 10:52:33 -0400

    wrote the add object for the class

--- ah_cll.h
+++ ah_cll.h
@@ -10,44 +10,70 @@ and efficient data storage.  */
 template <class T> class cll_node {
 public:
   T object;
-  void* next;
-  void* last;
+  cll_node<T>* next;
 
-  cll_node(T,void*,void*);
+  cll_node(T,cll_node<T>*);
 }
 
-template <class T> cll_node::cll_node(T _object,void* _next,void* _last){
+template <class T> cll_node::cll_node(T _object,cll_node<T>* _next){
   object = _object;
   next = _next;
-  last = _last;
 }
 
 template <class T> class ah_cll {
 public:
   int length;
-  void* first;
+  int size;
+  cll_node<T>* head;
 
   ah_cll(T, int);
+  ~ah_cll(void);
   void add(T);
-  void clear(void);
   T pop(void);
 
 private:
 
 };
 
-template <class T> ah_cll::ah_cll(T x,int length){
-  // First, allocate a place for the object
-  first = malloc(sizeof(cll_node))
+template <class T> ah_cll::ah_cll(int _length){
+  // Set the length
+  length = _length;
+  // initialize the head pointer to some value
+  head = NULL;
 }
 
-template <class T> void ah_cll::ah_cll(T x){
+template <class T> ah_cll::~ah_cll(void){
 
 }
 
-template <class T> void ah_cll::clear(void){
-
+template <class T> ah_cll::add(T _object){
+  // first, allocate memory for the object
+  cll_node<T>* node = (cll_node<T>*)malloc(sizeof(cll_node<T>));
+  // Then, set the object and the pointer into the node
+  node->object = _object;
+  // then, check if our length is over size
+  if (size + 1 > length){
+    // find the pointer from the first node
+    cll_node<T>* _next_head = head->next;
+    // delete the head node
+    free(head);
+    // connect the list to the "nexthead"
+    node->next = _next_head;
+    head = _next_head;
+  } else {
+    size = size + 1;
+    // just add the node
+    // if the list is only size one, link to itself
+    if (head == NULL) {
+      node->next = node;
+      head = node;
+    } else {
+      node->next = head;
+    }
+  }
 }
+
+
 
 template <class T> T ah_cll:pop(void){
   return x;
