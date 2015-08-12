88f1b508ab9eee43e88e8538ac766b28032f4768
Alex Hagen <ahagen@purdue.edu>
2015-08-12 13:50:17 -0400

    working templated class with insert, initializer, and pop methods

--- ah_cll.h
+++ ah_cll.h
@@ -1,82 +1,75 @@
-#pragma GCC diagnostic ignored "-Wwrite-strings"
 #ifndef ah_cll_h
 #define ah_cll_h
+
+#include <stdlib.h>
+#include <stdio.h>
 
 /*                        AH Circular Linked List Class                       */
 /*                             Author: Alex Hagen                             */
 /* This is an implementation of a linked list class that is circular for fast
 and efficient data storage.  */
 
-template <class T> class cll_node {
+template <class T> class node {
 public:
-  T object;
-  cll_node<T>* next;
-
-  cll_node(T,cll_node<T>*);
-}
-
-template <class T> cll_node::cll_node(T _object,cll_node<T>* _next){
-  object = _object;
-  next = _next;
-}
-
-template <class T> class ah_cll {
-public:
-  int length;
-  int size;
-  cll_node<T>* head;
-
-  ah_cll(T, int);
-  ~ah_cll(void);
-  void add(T);
-  T pop(void);
-
-private:
-
+  node(T,node<T>*);
+  T data;
+  node<T>* next;
 };
 
-template <class T> ah_cll::ah_cll(int _length){
-  // Set the length
+template <class T> node<T>::node(T _data, node<T>* _next){
+  data=_data;
+  next=_next;
+}
+
+template <class T> class cll {
+public:
+  cll(int);
+  void insert(T);
+  T pop();
+  int length;
+  int size;
+
+private:
+  node<T>* head;
+  node<T>* tail;
+};
+
+template <class T> cll<T>::cll(int _length){
   length = _length;
-  // initialize the head pointer to some value
-  head = NULL;
+  size = 1;
+  head = (node<T> *)malloc(sizeof(node<T>));
+  head->data = 0;
+  tail = head;
+  tail->next = head;
 }
 
-template <class T> ah_cll::~ah_cll(void){
-
+template <class T> void cll<T>::insert(T data)
+{
+        size = size + 1;
+        tail->next = (node<T> *)malloc(sizeof(node<T>));
+        tail = tail->next;
+        tail->data = data;
+        tail->next = head;
+        if (size > length) {
+          tail->next = head->next;
+          free(head);
+          head = tail->next;
+          size = size - 1;
+        }
 }
 
-template <class T> ah_cll::add(T _object){
-  // first, allocate memory for the object
-  cll_node<T>* node = (cll_node<T>*)malloc(sizeof(cll_node<T>));
-  // Then, set the object and the pointer into the node
-  node->object = _object;
-  // then, check if our length is over size
-  if (size + 1 > length){
-    // find the pointer from the first node
-    cll_node<T>* _next_head = head->next;
-    // delete the head node
+template <class T> T cll<T>::pop(void) {
+  T object;
+  if (size > 0){
+    object = head->data;
+    tail->next = head->next;
     free(head);
-    // connect the list to the "nexthead"
-    node->next = _next_head;
-    head = _next_head;
+    head = tail->next;
+    size = size - 1;
   } else {
-    size = size + 1;
-    // just add the node
-    // if the list is only size one, link to itself
-    if (head == NULL) {
-      node->next = node;
-      head = node;
-    } else {
-      node->next = head;
-    }
+    exit(-1);
   }
-}
-
-
-
-template <class T> T ah_cll:pop(void){
-  return x;
+  return(object);
 }
 
 #endif
